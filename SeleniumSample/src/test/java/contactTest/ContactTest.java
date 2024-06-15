058c09ec03e01f89ee1f98ef05845e2e79ecde4d
admin <admin@DESKTOP-ROIRK3H>
2024-06-15 14:31:20 +0530

    SeleniumSample1

---
+++ SeleniumSample/src/test/java/contactTest/ContactTest.java
@@ -0,0 +1,17 @@
+package contactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+@Test
+	
+	public void createContactTest()
+	{
+		System.out.println("createContactTest");
+	}
+	
+	public void createContactwithOrgTest()
+	{
+		System.out.println("createContactwithOrgTest");
+	}
+}
---
+++ SeleniumSample/src/test/java/org/OrgTest.java
@@ -0,0 +1,17 @@
+package org;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	
+	public void createContactTest()
+	{
+		System.out.println("createContactTest");
+	}
+	
+	public void createContactwithOrgTest()
+	{
+		System.out.println("createContactwithOrgTest");
+	}
+}
