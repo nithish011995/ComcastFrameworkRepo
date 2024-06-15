80d73b25bec14853461fb9e623bb8522379fec7f
rushm <rushm@LAPTOP-DENRBFPO>
2024-06-15 14:15:16 +0530

    Added a new class to existing framework

---
+++ ComcastCRMGUIFramework/src/test/java/Practice_TestNG/Rashmi_NewClassTest.java
@@ -0,0 +1,12 @@
+package Practice_TestNG;
+
+import org.testng.annotations.Test;
+
+public class Rashmi_NewClassTest {
+	@Test
+	public void rashmi_test()
+	{
+		System.out.println("Rashmi_New class in framework");
+	}
+
+}
